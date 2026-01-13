c6469cc29b5621acccf4bb63255b02966b935e29
Азат Валиуллин <azatkep118@gmail.com>
2026-01-13 16:23:14 +0300

    make working version

--- config/menu/views.py
+++ config/menu/views.py
@@ -1,42 +1,54 @@
-from django.shortcuts import render
+# menu/views.py
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Dish, Menu
 
 class DishListView(ListView):
     model = Dish
-    paginate_by = 10
+    template_name = 'menu/dish_list.html'
+    context_object_name = 'dishes'
 
     def get_queryset(self):
-        menu_id = self.kwargs.get("menu_id")
-        return Dish.objects.filter(menu_id=menu_id)
+        self.menu = Menu.objects.get(id=self.kwargs['menu_id'])
+        return Dish.objects.filter(menu=self.menu)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = Menu.objects.get(pk=self.kwargs.get("menu_id"))
+        context['menu'] = self.menu
         return context
-
 
 class DishCreateView(CreateView):
     model = Dish
-    fields = '__all__'
+    fields = ['name', 'price', 'description', 'image']
     template_name = 'menu/dish_form.html'
 
-    def get_initial(self):
-        return {'menu': self.kwargs.get("menu_id")}
+    def form_valid(self, form):
+        menu_id = self.kwargs['menu_id']
+        menu = Menu.objects.get(id=menu_id)
+        form.instance.menu = menu
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        menu_id = self.kwargs['menu_id']
+        context['menu'] = Menu.objects.get(id=menu_id)
+        return context
 
     def get_success_url(self):
         return reverse_lazy('dish_list', kwargs={'menu_id': self.object.menu.id})
-
 
 class DishUpdateView(UpdateView):
     model = Dish
-    fields = '__all__'
+    fields = ['name', 'price', 'description', 'image']
     template_name = 'menu/dish_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = self.object.menu
+        return context
 
     def get_success_url(self):
         return reverse_lazy('dish_list', kwargs={'menu_id': self.object.menu.id})
-
 
 class DishDeleteView(DeleteView):
     model = Dish
--- config/reviews/views.py
+++ config/reviews/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# reviews/views.py
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Review
+from menu.models import Dish
 
-# Create your views here.
+class ReviewCreateView(CreateView):
+    model = Review
+    fields = ['rating', 'comment']
+    template_name = 'reviews/review_form.html'
+
+    def form_valid(self, form):
+        dish_id = self.kwargs['dish_id']
+        dish = Dish.objects.get(id=dish_id)
+        form.instance.dish = dish
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['dish'] = Dish.objects.get(id=self.kwargs['dish_id'])
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('dish_list', kwargs={'menu_id': self.object.dish.menu.id})
